739e43105ac238870467150b54d1a184c68ec9ff
NicolasCastonguay <nicolas.castonguay.3@ulaval.ca>
2024-05-30 15:15:42 -0400

    pour que ca build

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,10 @@
-import TopMenu from "@/components/layout/top-menu/top-menu";
+// import TopMenu from "@/components/layout/top-menu/top-menu";
 
-export default function HomePage() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-10">
-      <TopMenu />
-      <h1 className="text-3xl font-bold">Home Page</h1>
-    </div>
-  );
-}
+// export default function HomePage() {
+//   return (
+//     <div className="flex h-screen flex-col items-center justify-center space-y-10">
+//       <TopMenu />
+//       <h1 className="text-3xl font-bold">Home Page</h1>
+//     </div>
+//   );
+// } 
