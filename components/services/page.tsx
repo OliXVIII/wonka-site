41735128d63c34062f7273397ced128782568a91
NicolasCastonguay <nicolas.castonguay.3@ulaval.ca>
2024-06-04 15:19:36 -0400

    servicessssssssss

--- components/services/page.tsx
+++ components/services/page.tsx
@@ -9,28 +9,28 @@ type ServicesPageProps = {
 
 export const ServicesPage = ({ uiContent, storage }: ServicesPageProps) => {
   return (
-    <div className="my-8 flex flex-col items-center justify-center space-y-8">
-      {uiContent.services?.services?.map((service, index) => (
-        <div
-          key={service.title}
-          className={`flex w-full p-5 ${
-            index % 2 === 0 ? "flex-row" : "flex-row-reverse"
-          } space-x-5`}
-        >
-          <div className="relative aspect-video min-w-[50%]">
-            <Image
-              src={service.image}
-              alt={service.title}
-              layout="fill"
-              className="object-cover rounded-lg shadow-lg"
-            />
+    <div className="container mx-auto my-8 px-4">
+      <div className="grid gap-8">
+        {uiContent.services?.services?.map((service, index) => (
+          <div
+            key={service.title}
+            className={`flex flex-col md:flex-row ${index % 2 !== 0 && "md:flex-row-reverse"} shadow-lg rounded-lg overflow-hidden dark:shadow-sm dark:shadow-white`}
+          >
+            <div className="relative md:w-1/2 w-full aspect-video">
+              <Image
+                src={service.image}
+                alt={service.title}
+                layout="fill"
+                className="object-cover"
+              />
+            </div>
+            <div className="flex flex-col justify-center p-8 md:w-1/2 w-full ">
+              <h3 className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-200 mb-4">{service.title}</h3>
+              <p className="text-gray-600 dark:text-gray-400 text-lg">{service.description}</p>
+            </div>
           </div>
-          <div className="flex flex-col justify-center p-5 space-y-4">
-            <h3 className="text-center text-2xl font-semibold">{service.title}</h3>
-            <p className="text-gray-300">{service.description}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
