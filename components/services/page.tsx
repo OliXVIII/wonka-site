40651afbe9802fc1cde4c6e3515ab1cf4838e33c
NicolasCastonguay <nicolas.castonguay.3@ulaval.ca>
2024-06-04 14:27:41 -0400

    petit ajustement

--- components/services/page.tsx
+++ components/services/page.tsx
@@ -9,28 +9,28 @@ type ServicesPageProps = {
 
 export const ServicesPage = ({ uiContent, storage }: ServicesPageProps) => {
   return (
-    <div className="my-8 flex flex-col items-center justify-center">
-      {uiContent.services?.services?.map((service, index) => {
-        return (
-          <div
-            key={service.title}
-            className={`flex w-full ${index % 2 === 0 ? "" : "flex-row-reverse"}`}
-          >
-            <div className="relative aspect-video min-w-[50%]">
-              <Image
-                src={service.image}
-                alt={service.title}
-                fill
-                className="object-cover"
-              />
-            </div>
-            <div className="p-5">
-              <h3 className="text-center text-2xl">{service.title}</h3>
-              <p>{service.description}</p>
-            </div>
+    <div className="my-8 flex flex-col items-center justify-center space-y-8">
+      {uiContent.services?.services?.map((service, index) => (
+        <div
+          key={service.title}
+          className={`flex w-full p-5 ${
+            index % 2 === 0 ? "flex-row" : "flex-row-reverse"
+          } space-x-5`}
+        >
+          <div className="relative aspect-video min-w-[50%]">
+            <Image
+              src={service.image}
+              alt={service.title}
+              layout="fill"
+              className="object-cover rounded-lg shadow-lg"
+            />
           </div>
-        );
-      })}
+          <div className="flex flex-col justify-center p-5 space-y-4">
+            <h3 className="text-center text-2xl font-semibold">{service.title}</h3>
+            <p className="text-gray-300">{service.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
