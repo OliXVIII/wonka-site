fa19514b80cfd65ed501811be3a74efb7c7547fa
OliXVIII <olioli44@hotmail.com>
2024-06-06 12:21:33 -0400

    P'tit fix #2

--- components/upcoming-event/page/main-image.tsx
+++ components/upcoming-event/page/main-image.tsx
@@ -49,7 +49,7 @@ export const MainImageUpcoming = ({
             src={upcomingEvent.logo.image}
             width={200}
             height={200}
-            className="absolute left-6 top-0 z-50 aspect-auto h-36 w-fit rounded-b-box object-cover opacity-45 dark:invert max-sm:h-20"
+            className="absolute left-6 top-0 z-50 aspect-auto h-36 w-auto w-fit rounded-b-box object-cover opacity-45 dark:invert max-sm:h-20"
           />
         </>
       )}
